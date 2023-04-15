49f0f004de893c19d273bed58f59a556e44b92b9
VisionAutomationTesting <visioncorporatetraining@gmail.com>
2023-04-15 10:51:27 +0530

    changes ased by reviewer

--- src/main/java/com/policybazar/pages/QuotesPage.java
+++ src/main/java/com/policybazar/pages/QuotesPage.java
@@ -2,10 +2,7 @@ package com.policybazar.pages;
 
 public class QuotesPage {
 
-	public static void main(String[] args) {
-
-		System.out.println("demo changes!!!");
-
+	public QuotesPage() {
+		
 	}
-
 }
