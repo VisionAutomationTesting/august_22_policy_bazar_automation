537db80bece11b9fd82a6c187ffa4e5a69740b97
VisionAutomationTesting <97270304+VisionAutomationTesting@users.noreply.github.com>
2023-04-15 11:11:35 +0530

    Update QuotesPage.java
    friend commit

--- src/main/java/com/policybazar/pages/QuotesPage.java
+++ src/main/java/com/policybazar/pages/QuotesPage.java
@@ -3,6 +3,10 @@ package com.policybazar.pages;
 public class QuotesPage {
 
 	public QuotesPage() {
-		
+		System.out.prinln("constructor");
+	}
+	
+	public static void m1(){
+		System.out.prinln("m1");
 	}
 }
